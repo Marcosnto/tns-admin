9dee3f5dc48c06dda241de2d44dccaeb9244079e
Marcos Neto <marcos_nto@hotmail.com>
2023-03-05 18:46:23 -0300

    chore: add favicon and set image typescript modules

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -1,6 +1,11 @@
+import favicon from './extensions/favicon.png'
+
 export default {
   config: {
     locales: ['pt-BR'],
+  },
+  head: {
+    favicon: favicon,
   },
   bootstrap(app) {
     console.log(app);
