97b816a9916d0c22fdba9973d29eee56243c3f8b
Marcos Neto <marcos_nto@hotmail.com>
2023-03-05 18:53:30 -0300

    chore: remove tutorial and notificacions release

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -3,6 +3,10 @@ import favicon from './extensions/favicon.png'
 export default {
   config: {
     locales: ['pt-BR'],
+    tutorial: false,
+    notifications: {
+      release: false,
+    },
   },
   head: {
     favicon: favicon,
