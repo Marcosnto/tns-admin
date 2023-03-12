80d03020d6ff5569947e62c7b5d1e52dc72ad975
Marcos Neto <marcos_nto@hotmail.com>
2023-03-12 18:43:47 -0300

    feat: overwrite En translations removing 'strapi' term

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -9,10 +9,14 @@ export default {
     },
     translations: {
       en: {
-        'app.components.LeftMenu.navbrand.title': 'Dashboard'
+        'app.components.LeftMenu.navbrand.title': 'Dashboard',
+        'Auth.form.welcome.title': 'Welcome!',
+        'Auth.form.welcome.subtitle': 'Login in to your account',
+        'Auth.form.register.subtitle': "Credentials are only used to authenticate. All saved data will be stored in your database."
       },
       'pt-BR': {
-        'app.components.LeftMenu.navbrand.title': 'Painel'
+        'app.components.LeftMenu.navbrand.title': 'Painel',
+        'Auth.form.welcome.subtitle': 'Acesse a sua conta',
       }
     },
     head: {
