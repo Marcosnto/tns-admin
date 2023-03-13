866f91bd49c355a4375856ef44c36c4998981dda
Marcos Neto <marcos_nto@hotmail.com>
2023-03-12 23:31:57 -0300

    feat: create privacy and politics  single type

---
+++ src/api/politicas-de-privacidade/controllers/politicas-de-privacidade.ts
@@ -0,0 +1,7 @@
+/**
+ * politicas-de-privacidade controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::politicas-de-privacidade.politicas-de-privacidade');
---
+++ src/api/politicas-de-privacidade/routes/politicas-de-privacidade.ts
@@ -0,0 +1,7 @@
+/**
+ * politicas-de-privacidade router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::politicas-de-privacidade.politicas-de-privacidade');
