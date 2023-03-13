73870ba8afa869141ba95f40075e83a18fd6f999
Marcos Neto <marcos_nto@hotmail.com>
2023-03-12 23:31:34 -0300

    feat: create terms and services single type

---
+++ src/api/termo-de-servico/controllers/termo-de-servico.ts
@@ -0,0 +1,7 @@
+/**
+ * termo-de-servico controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::termo-de-servico.termo-de-servico');
---
+++ src/api/termo-de-servico/routes/termo-de-servico.ts
@@ -0,0 +1,7 @@
+/**
+ * termo-de-servico router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::termo-de-servico.termo-de-servico');
